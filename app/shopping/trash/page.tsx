fa40412abc2ed665a13687b35a3fc64c3abf332d
Muzamil-Shah <muzamilshahqurishy782787@gmail.com>
2024-09-09 01:06:05 +0530

    remove trash page

--- app/shopping/trash/page.tsx
+++ app/shopping/trash/page.tsx
@@ -1,11 +1,6 @@
 import { useQuery } from "convex/react";
-import { FileBrowsers } from "../_component/product-browser";
 import { api } from "@/convex/_generated/api";
 
 export default function TrashPage() {
-  return (
-    <>
-      <FileBrowsers title={"Trash"} deletedOnly />
-    </>
-  );
+  return <></>;
 }
